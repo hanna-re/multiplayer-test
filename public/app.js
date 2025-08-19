021afd1d6f882cd9fc4a6f060a9822924ae0b15c
hanna-re <rebryna.hanna@gmail.com>
2025-08-19 22:39:43 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 async function callBackend() {
-    const response = await fetch('/.netlify/functions/flask_app/api/hello');
-    const data = await response.json();
-    document.getElementById('response').innerText = data.message;
+  const response = await fetch('https://backend-test-murex.vercel.app/api/hello');
+  const data = await response.json();
+  document.getElementById('response').innerText = data.message;
 }
